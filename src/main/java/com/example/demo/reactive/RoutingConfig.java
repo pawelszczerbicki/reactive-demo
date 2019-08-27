dff8ac16f7e4152efe062da807c5d3f84978c368
Patryk Zieliński <patrykzielu@gmail.com>
2019-08-27 17:20:42 +0200

    Safe side effect enclose to Monad Mono

--- src/main/java/com/example/demo/reactive/RoutingConfig.java
+++ src/main/java/com/example/demo/reactive/RoutingConfig.java
@@ -1,10 +1,11 @@
 package com.example.demo.reactive;
 
-import com.example.demo.Bind;
+import com.example.demo.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
@@ -16,9 +17,16 @@ public class RoutingConfig {
     @Bean
     RouterFunction<ServerResponse> routing(ReactiveRepository repository) {
         return route().POST("/", s -> repository.byId(s.pathVariable("id"))
-                .flatMap(d -> s.bodyToMono(Bind.class).flatMap(b -> just(d.bind(b))))
+                .flatMap(user -> s.bodyToMono(Bind.class).flatMap(bind -> updateBind(user, bind)))
                 .flatMap(repository::save)
                 .then(ok().build()))
                 .build();
+    }
+
+    private Mono<User> updateBind(User user, Bind newBind){
+        return Mono.fromCallable(() ->{
+            user.bind(newBind);
+            return user;
+        });
     }
 }
